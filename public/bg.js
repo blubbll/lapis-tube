dd0c6290f8181a5f1e08ba37a32547d46b60bbe3
Glitch (lapis-tube) <none>
2020-01-17 09:28:59 +0000

    better bars, prep for livesearch

---
+++ public/bg.js
@@ -0,0 +1,31 @@
+//BG START
+const initBg = () => {
+  var rn = Math.floor(Math.random() * 150 + 60);
+  var rs = Math.floor(Math.random() * 11 + 4);
+  const prim = getComputedStyle(document.documentElement).getPropertyValue(
+    "--color-accent-main"
+  );
+
+  var t = new Trianglify({
+    x_gradient: [
+      "#173b5e",
+      "#3e87d0",
+      "#1c4873",
+      "#215487",
+      "#26619c",
+      "#2b6eb1",
+      "#307bc5"
+    ], //Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+  });
+  var pattern = t.generate(window.innerWidth, window.innerHeight);
+  document.body.setAttribute("style", `background-image: ${pattern.dataUrl}`);
+
+  setTimeout(() => {
+    document.querySelector("diamond").setAttribute("style", "display:none;");
+    document.querySelector("#wrap").setAttribute("style", "");
+  }, 1499);
+};
+window.onresize = debounce(initBg, 49);
+//BG END
--- public/client.js
+++ public/client.js
@@ -2,98 +2,59 @@ window.ln = navigator.language || navigator.userLanguage;
 
 const { autoComplete } = window;
 
-//BG START
-const initBg = () => {
-  var rn = Math.floor(Math.random() * 150 + 60);
-  var rs = Math.floor(Math.random() * 11 + 4);
-  const prim = getComputedStyle(document.documentElement).getPropertyValue(
-    "--color-accent-main"
-  );
+//DEBOUNHCE
+function debounce(func, wait, immediate) {
+  var timeout;
 
-  var t = new Trianglify({
-    x_gradient: [
-      "#173b5e",
-      "#3e87d0",
-      "#1c4873",
-      "#215487",
-      "#26619c",
-      "#2b6eb1",
-      "#307bc5"
-    ], //Trianglify.colorbrewer.Spectral[rs],
-    noiseIntensity: 0,
-    cellsize: rn
-  });
-  var pattern = t.generate(window.innerWidth, window.innerHeight);
-  document.body.setAttribute("style", `background-image: ${pattern.dataUrl}`);
-};
-window.onresize = initBg;
-//BG END
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
 
-new autoComplete({
-  data: {
-    // Data src [Array, Function, Async] | (REQUIRED)
-    src: async () => {
-      // API key token
-      // User search query
-      const query = document.querySelector("#autoComplete").value;
-      // Fetch External Data Source
-      const source = await fetch(
-        `https://www.food2fork.com/api/search?key=${token}&q=${query}`
-      );
-      // Format data into JSON
-      const data = await source.json();
-      // Return Fetched data
-      return data.recipes;
-    },
-    key: ["title"],
-    cache: false
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
+
+var countries = [
+  { label: "United Kingdom", value: "UK" },
+  { label: "United States", value: "US" }
+];
+
+//SEARCH
+var input = document.getElementById("search");
+
+autocomplete({
+  input: input,
+  showOnFocus: 1,
+  className: "live-search",
+  debounceWaitMs: 500,
+  fetch: function(text, update) {
+    //input
+    text = text.toLowerCase();
+
+    fetch("/api/complete/de:" + text)
+      .then(res => res.text())
+      .then(arrText => {
+        const suggs = [];
+        console.log(JSON.parse(arrText))
+        Array.from(arrText).forEach(function(sugg) {
+          //suggs.push()
+          console.log(sugg);
+          //update(suggs)
+        });
+      });
   },
-  query: {
-    // Query Interceptor               | (Optional)
-    manipulate: query => {
-      return query.replace("pizza", "burger");
-    }
-  },
-  sort: (a, b) => {
-    // Sort rendered results ascendingly | (Optional)
-    if (a.match < b.match) return -1;
-    if (a.match > b.match) return 1;
-    return 0;
-  },
-  placeHolder: "Food & Drinks...", // Place Holder text                 | (Optional)
-  selector: "#autoComplete", // Input field selector              | (Optional)
-  threshold: 3, // Min. Chars length to start Engine | (Optional)
-  debounce: 300, // Post duration for engine to start | (Optional)
-  searchEngine: "strict", // Search Engine type/mode           | (Optional)
-  resultsList: {
-    // Rendered results list object      | (Optional)
-    render: true,
-    container: source => {
-      source.setAttribute("id", "food_list");
-    },
-    destination: document.querySelector("#autoComplete"),
-    position: "afterend",
-    element: "ul"
-  },
-  maxResults: 5, // Max. number of rendered results | (Optional)
-  highlight: true, // Highlight matching results      | (Optional)
-  resultItem: {
-    // Rendered result item            | (Optional)
-    content: (data, source) => {
-      source.innerHTML = data.match;
-    },
-    element: "li"
-  },
-  noResults: () => {
-    // Action script on noResults      | (Optional)
-    const result = document.createElement("li");
-    result.setAttribute("class", "no_result");
-    result.setAttribute("tabindex", "1");
-    result.innerHTML = "No Results";
-    document.querySelector("#autoComplete_list").appendChild(result);
-  },
-  onSelection: feedback => {
-    // Action script onSelection event | (Optional)
-    console.log(feedback.selection.value.image_url);
+  onSelect: function(item) {
+    input.value = item.label;
   }
 });
