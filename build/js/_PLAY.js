1f3b0dc961727b8b16b1d9f04d2c1c04bae1e22c
Glitch (lapis-tube) <none>
2020-02-05 13:37:42 +0000

    format stream data

--- build/js/_PLAY.js
+++ build/js/_PLAY.js
@@ -1,78 +1,335 @@
-const {
-  autocomplete,
-  alert,
-  createThumbs,
-  debounce,
-  done,
-  fetch,
-  getL,
-  getSize,
-  HOST,
-  REGION,
-  load,
-  moment,
-  T,
-  waitForElement
-} = window;
+{
+  const {
+    autocomplete,
+    alert,
+    createThumbs,
+    debounce,
+    done,
+    fetch,
+    getL,
+    getSize,
+    HOST,
+    REGION,
+    load,
+    moment,
+    speed,
+    T,
+    afterglow,
+    Browser,
+    Fullscreen,
+    waitForElement
+  } = window;
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
 
-//afterglow player
-const { afterglow, vjs, poster } = window;
+  //afterglow player
 
-let { Player } = window;
+  let { Player } = window;
 
-Player = {
-  open: () => {
-    $("#view-inner").insertAdjacentHTML("beforeend", T.PLAYER);
-    $("#view-inner").classList.remove("wait");
-    $("#filters").style.setProperty("display", "none", "important");
-    $("#results").style.setProperty("display", "none", "important");
-  },
-  close: () => {
-    $("#player").remove();
-    $("#filters").style.setProperty("display", "");
-    $("#results").style.setProperty("display", "");
-  },
-  play: vid => {
-    Player.open();
-    afterglow.initVideoElements();
-    fetch(`${HOST}/api/${REGION}/video/${vid}`)
-      .then(res => res.text())
-      .then(raw => {
-        let vid = JSON.parse(raw);
-        let HTML = "";
+  Player = {
+    open: () => {
+      $("#view-inner").insertAdjacentHTML("beforeend", T.PLAYER);
+      $("#view-inner").classList.remove("wait");
+      $("#filters").style.setProperty("display", "none", "important");
+      $("#results").style.setProperty("display", "none", "important");
+    },
+    close: () => {
+      $("#player").remove();
+      $("#filters").style.setProperty("display", "");
+      $("#results").style.setProperty("display", "");
+    },
+    openFromResult: that => {
+      $("#view-inner").classList.add("wait");
 
-        waitForElement(".vjs-poster").then(el => {
-          el.outerHTML = $("#fake-poster").innerHTML.replace(
-            "{{preview-set}}",
+      $("#results").classList.add("grow");
+      that.classList.add("growing");
+
+      setTimeout(() => {
+        Player.play(that.getAttribute("video-id"));
+
+        $("#results").classList.remove("grow");
+        that.classList.remove("growing");
+      }, 799);
+    },
+    play: vid => {
+      Player.open();
+      afterglow.initVideoElements();
+      let VIDEO = $("video");
+      const AUDIO = $("audio");
+
+      fetch(`${HOST}/api/${REGION}/video/${vid}`)
+        .then(res => res.text())
+        .then(raw => {
+          let vid = JSON.parse(raw);
+          let HTML = "";
+
+          const TITLE = vid.title;
+
+          $("poster>img").setAttribute(
+            "srcset",
             createThumbs(vid.videoThumbnails)
           );
+
+          const STREAM = {
+            low: vid.formatStreams[0],
+            AUDIOS: [],
+            VIDEOS: [],
+            CURRENTSTREAM: {}
+          };
+
+          for (const format of vid.adaptiveFormats) {
+            if (format.type.startsWith("video")) STREAM.VIDEOS.push(format);
+            if (format.type.startsWith("audio")) STREAM.AUDIOS.push(format);
+          }
+
+          //sort by bitrate
+          STREAM.AUDIO = STREAM.AUDIO.sort((a, b) => a.bitrate - b.bitrate);
+          STREAM.VIDEO = STREAM.VIDEO.sort((a, b) => a.bitrate - b.bitrate);
+
+          switch (speed.speed) {
+            case "slow":
+              {
+                STREAM.CURRENTSTREAM.AUDIO = STREAM.AUDIOS[0];
+                STREAM.CURRENTSTREAM.VIDEO = STREAM.VIDEOS[0];
+              }
+              break;
+            case "medium":
+              {
+                STREAM.CURRENTSTREAM.AUDIO =
+                  STREAM.AUDIOS[Math.floor(STREAM.AUDIOS.length - 1 / 2)];
+                STREAM.CURRENTSTREAM.VIDEO =
+                  STREAM.VIDEOS[Math.floor(STREAM.VIDEOS.length - 1 / 2)];
+              }
+              break;
+            case "fast":
+              {
+                STREAM.CURRENTSTREAM.AUDIO =
+                  STREAM.AUDIOS[STREAM.AUDIOS.length - 1];
+                STREAM.CURRENTSTREAM.VIDEO =
+                  STREAM.VIDEOS[STREAM.VIDEOS.length - 1];
+              }
+              break;
+          }
+
+          if (Browser.isFirefox || Browser.isChrome) {
+            console.log(vid);
+
+            const poster = $("poster>img").currentSrc;
+
+            $(
+              "video"
+            ).outerHTML = `<video autoplay preload="metadata" class="afterglow" height="0" width="0" poster="${poster}"></video>`;
+
+            VIDEO = $("video");
+
+            //SET MEDIA
+            AUDIO.src = STREAM.CURRENTSTEAM.AUDIO;
+
+            VIDEO.src = STREAM.CURRENTSTREAM.VIDEO;
+
+            //init afterglow
+            afterglow.initVideoElements();
+
+            {
+              //MIRROR VOLUME
+              Object.defineProperty(VIDEO, "volume", {
+                set: val => {
+                  VIDEO._volume = val;
+                  //!
+                  AUDIO.volume = val;
+                },
+                get: () => {
+                  return VIDEO._volume;
+                }
+              });
+
+              //MIRROR MUTED
+              Object.defineProperty(VIDEO, "muted", {
+                set: val => {
+                  VIDEO._muted = val;
+                  //!
+                  AUDIO.muted = val;
+                },
+                get: () => {
+                  return VIDEO._muted;
+                }
+              });
+
+              //SYNC PLAY
+              VIDEO._play = VIDEO.play;
+              Object.defineProperty(VIDEO, "play", {
+                get: () => {
+                  AUDIO.paused && AUDIO.play();
+
+                  if (AUDIO.paused) {
+                    console.debug("muted");
+                    $("#mep_0").insertAdjacentHTML(
+                      "afterbegin",
+                      "<lapis-muted></lapis-muted>"
+                    );
+
+                    const MUTED_WARNING = $("lapis-muted");
+                    MUTED_WARNING.innerHTML =
+                      "THE VIDEO IS MUTED. CLICK ME TO UNMUTE";
+                    MUTED_WARNING.style.display = "flex";
+                    MUTED_WARNING.addEventListener("click", () => {
+                      MUTED_WARNING.remove();
+                      AUDIO.muted = false;
+                      VIDEO.currentTime = 0;
+                      AUDIO.play();
+                      !AUDIO.muted &&
+                        !AUDIO.paused &&
+                        console.debug("unmuted.");
+                    });
+                  }
+
+                  return VIDEO._play;
+                }
+              });
+              //SYNC PAUSE
+              VIDEO._pause = VIDEO.pause;
+              Object.defineProperty(VIDEO, "pause", {
+                get: () => {
+                  AUDIO.pause();
+                  return VIDEO._pause;
+                }
+              });
+            }
+
+            VIDEO.addEventListener(
+              "loadedmetadata",
+              e => {
+                console.log("loaded");
+
+                try {
+                  VIDEO.play();
+                } catch (e) {
+                  console.warn(e);
+                }
+
+                //fancy title
+                $(".afterglow__controls").insertAdjacentHTML(
+                  "afterbegin",
+                  `<div class="afterglow__title-bar">${TITLE}</div>`
+                );
+                //fancy control area
+                $(".afterglow__controls").insertAdjacentHTML(
+                  "beforeend",
+                  `<div class="afterglow__control-bar"></div>`
+                );
+
+                const fitRatio = () => {
+                  const that = e.target;
+
+                  const ratio =
+                    $("lapis-player").getAttribute("ratio") ||
+                    that.videoHeight / that.videoWidth;
+                  $("lapis-player").setAttribute("ratio", ratio);
+                  $("#mep_0").style.height = `${$("#mep_0").clientWidth *
+                    ratio}px`;
+                };
+                setTimeout(fitRatio, 0);
+                window.addEventListener("resize", fitRatio);
+              },
+              false
+            );
+
+            //load vidn
+            VIDEO.load();
+
+            try {
+              //$("video").play();
+            } catch (e) {}
+            {
+            }
+
+            //keep video time in sync with audio
+            const syncer = setInterval(() => {
+              console.log(AUDIO.currentTime / VIDEO.currentTime);
+              AUDIO.currentTime / VIDEO.currentTime > 1.1 && [
+                (VIDEO.currentTime = AUDIO.currentTime)
+              ];
+            }, 2999);
+
+            //MOBILE DEVICE
+            if (typeof window.orientation !== "undefined") {
+              document.title = TITLE;
+
+              //sync audio pause on android video pause
+              document.addEventListener(
+                "fullscreenchange",
+                () => {
+                  if (!document.fullscreen && VIDEO.pause && !AUDIO.paused)
+                    AUDIO.pause();
+                },
+                false
+              );
+
+              //sync video war playNpause with audio
+              VIDEO.addEventListener("play", () => {
+                AUDIO.play();
+              }),
+                VIDEO.addEventListener("pause", () => {
+                  !AUDIO.paused && AUDIO.pause();
+                });
+              //: @android: FIXME
+
+              //RESET EVENT
+              $(".afterglow__top-control-bar").innerHTML = $(
+                ".afterglow__top-control-bar"
+              ).innerHTML;
+              //ADD NEW EVENT
+              $(
+                ".afterglow__button.afterglow__fullscreen-toggle"
+              ).addEventListener("click", e => {
+                const that = e.target;
+
+                if (document.fullscreen) {
+                  Fullscreen.exit();
+                } else {
+                  setTimeout(() => {
+                    $("#mep_0").classList.remove("afterglow__container");
+                    Fullscreen.enter($("video"));
+                  });
+                }
+              });
+            }
+
+            //IOS
+          } /* if (
+      !!navigator.platform &&
+      /iPad|iPhone|iPod/.test(navigator.platform)
+    ) */ else {
+            {
+              const VIDEO = $("video");
+              const AUDIO = $("audio");
+
+              document.title = TITLE;
+
+              //sync audio playNpause with video
+              AUDIO.addEventListener("play", () => {
+                VIDEO.paused && VIDEO.play();
+              }),
+                AUDIO.addEventListener("pause", () => {
+                  !VIDEO.paused && VIDEO.pause();
+                });
+
+              //sync video playNpause with audio
+              VIDEO.addEventListener("play", () => {
+                AUDIO.paused && AUDIO.play();
+              }),
+                VIDEO.addEventListener("pause", () => {
+                  !AUDIO.paused && AUDIO.pause();
+                });
+            }
+
+            $("video").outerHTML =
+              "<video preload controls playsinline></video>";
+
+            $("video").src = STREAM.LOW;
+          }
         });
-
-        console.log(vid);
-      });
-  }
-};
-
-document.addEventListener("click", e => {
-  const that = $(".card[video-id]");
-  if (!that || (that && !that.contains(e.target))) {
-    e.preventDefault();
-    return false;
-  }
-  /////////////////////////////
-
-  $("#view-inner").classList.add("wait");
-
-  $("#results").classList.add("grow");
-  that.classList.add("growing");
-
-  setTimeout(() => {
-    Player.play(that.getAttribute("video-id"));
-
-    $("#results").classList.remove("grow");
-    that.classList.remove("growing");
-  }, 799);
-});
+    }
+  };
+}
--- build/js/tools.js
+++ build/js/tools.js
@@ -2,7 +2,15 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const { NProgress } = window;
-let { createThumbs, debounce, fetch, getL, getSize, loadImage } = window;
+let {
+  createThumbs,
+  debounce,
+  fetch,
+  Fullscreen,
+  getL,
+  getSize,
+  loadImage
+} = window;
 
 function detectIEEdge() {
   const ua = window.navigator.userAgent;
@@ -152,3 +160,183 @@ getL = () => {
     });
   };
 }
+
+/*
+tools
+*/
+Fullscreen = {
+  enter: elem => {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  },
+  exit: () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE/Edge */
+      document.msExitFullscreen();
+    }
+  }
+};
+
+HTMLElement.prototype.onEvent = (eventType, callBack, useCapture) => {
+  this.addEventListener(eventType, callBack, useCapture);
+  if (!this.myListeners) {
+    this.myListeners = [];
+  }
+  this.myListeners.push({ eType: eventType, callBack: callBack });
+  return this;
+};
+
+HTMLElement.prototype.removeListeners = () => {
+  if (this.myListeners) {
+    for (var i = 0; i < this.myListeners.length; i++) {
+      this.removeEventListener(
+        this.myListeners[i].eType,
+        this.myListeners[i].callBack
+      );
+    }
+    delete this.myListeners;
+  }
+};
+
+{
+  let that = {};
+  // Opera 8.0+
+  that.isOpera =
+    (!!window.opr && !!window.opr.addons) ||
+    !!window.opera ||
+    navigator.userAgent.indexOf(" OPR/") >= 0;
+
+  // Firefox 1.0+
+  that.isFirefox = typeof window.InstallTrigger !== "undefined";
+
+  // Safari 3.0+ "[object HTMLElementConstructor]"
+  that.isSafari =
+    /constructor/i.test(window.HTMLElement) ||
+    (function(p) {
+      return p.toString() === "[object SafariRemoteNotification]";
+    })(
+      !window["safari"] ||
+        (typeof window.safari !== "undefined" && window.safari.pushNotification)
+    );
+
+  // Internet Explorer 6-11
+  that.isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
+  // Edge 20+
+  that.isEdge = !that.isIE && !!window.StyleMedia;
+
+  // Chrome 1 - 71
+  that.isChrome =
+    !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+  // Edge (based on chromium) detection
+  that.isEdgeChromium =
+    that.isChrome && navigator.userAgent.indexOf("Edg") != -1;
+
+  // Blink engine detection
+  that.isBlink = (that.isChrome || that.isOpera) && !!window.CSS;
+  window.Browser = that;
+}
+
+const speed = {
+  _i: 0,
+  _arrTimes: [],
+  doTest() {
+    const that = this; //https://stackoverflow.com/a/19448729
+    const r = String.fromCharCode(
+      0x30a0 + Math.random() * (0x30ff - 0x30a0 + 1)
+    ); //-
+    let dummyImage = new Image();
+    const tStart = new Date().getTime();
+    if (this._i < that.config.times) {
+      that._i++;
+      console.debug(`Test ${this._i} of ${that.config.times}...`);
+      dummyImage.src = `${that.config.image}?t=${tStart}`;
+      dummyImage.onload = () => {
+        const tEnd = new Date().getTime();
+        const tTimeTook = tEnd - tStart;
+        that._arrTimes[that._i] = tTimeTook;
+        that.doTest();
+        dummyImage = null;
+      };
+    } else {
+      /** calculate average of array items then callback */
+      const sum = this._arrTimes.reduce((a, b) => a + b);
+      const avg = sum / this._arrTimes.length;
+      this.setResult(avg);
+    }
+  },
+  setResult(avg, speed) {
+    const that = this;
+    if (!speed) {
+      if (avg >= 250) speed = "slow";
+      if (avg >= 150 && avg <= 250) speed = "medium";
+      if (avg <= 150) speed = "fast";
+    }
+    let txt;
+    if (avg) txt = `Time: ${avg.toFixed(2)}ms - speed: ${speed}`;
+    else txt = `speed: ${speed}`;
+    this.speed = speed;
+    console.debug("Test done:");
+    console.debug({ avg, speed });
+    console.debug("\n");
+    that._i = 0;
+    that._arrTimes = [];
+    if (that._loop)
+      setTimeout(() => {
+        console.debug("Testing...");
+        that.doTest();
+      }, that.config.interval);
+  },
+  stopLoop() {
+    this._loop = false;
+    return "loop stopped";
+  },
+  startLoop() {
+    console.debug("Testing...");
+    this._loop = true;
+    let speed;
+    this._i = 0;
+    /** output */
+    const conn =
+      navigator.connection ||
+      navigator.mozConnection ||
+      navigator.webkitConnection;
+    if (false && conn) {
+      if (conn.saveData) speed = "slow";
+      else if (navigator.connection.rtt)
+        this.setResult(navigator.connection.rtt, speed);
+      //fallback to image
+    } else {
+      this._i = 0;
+      this.doTest(function(avg) {
+        this.setResult(avg);
+      });
+    }
+    return "loop started";
+  }
+};
+speed.config = {
+  image: "//blubbll.b-cdn.net/speed.jpg",
+  times: 3, //times to check
+  interval: 60000
+};
+speed.startLoop();
