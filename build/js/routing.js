38ced2a2d0aa6d46267b05fcf51b19eea0682df4
Glitch (lapis-tube) <none>
2020-03-03 15:56:22 +0000

    make yt-link visit abortable (may need some work)

--- build/js/routing.js
+++ build/js/routing.js
@@ -1,7 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const { Player, waitForElement } = window;
+const { HOST, Player, waitForElement, setActiveView } = window;
 let { route } = window;
 
 route = to => {
@@ -23,12 +23,15 @@ route = to => {
         default: {
           if (to.startsWith("/watch?v=")) {
             const id = to.split("/watch?v=")[1];
-
+        
             //from yt vid
             console.debug(
               "attempting to play requested video (direct call from yt)",
               id
             );
+             setActiveView("start")
+            history.pushState(null, null, HOST);
+           
             Player.play(id);
           }
         }
